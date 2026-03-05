38bd08e30ac2519ca01909beb194d7eb6e2629dd
Jorge <jg4@iastate.edu>
2026-03-05 16:30:45 -0600

    fixes to get code to compile well

--- cmake/compiler_flags_NVHPC_Fortran.cmake
+++ cmake/compiler_flags_NVHPC_Fortran.cmake
@@ -2,12 +2,12 @@
 set(r8_flags "-r8") # Fortran flags for 64BIT precision
 
 # NVHPC Fortan
-set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -O2 -Mnovect  -gpu=mem:separate -Mnofma  -mp=gpu -acc=gpu")
+set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -O2 -Mnovect  -gpu=mem:separate -Mnofma -stdpar=gpu -mp=gpu -acc=gpu -gpu=mem:separate")
 
-set(CMAKE_Fortran_FLAGS_RELEASE "-O3")
+set(CMAKE_Fortran_FLAGS_RELEASE "")
 
 set(CMAKE_Fortran_FLAGS_DEBUG "-O0" )
 # -g can cause bugs, see: https://forums.developer.nvidia.com/t/bug-compiling-with-g-o0-produces-a-compute-sanitizer-error-removing-g-removes-the-error/341478
 # not sure if this is only on GPUs
 
-set(CMAKE_Fortran_LINK_FLAGS "" )
+set(CMAKE_Fortran_LINK_FLAGS "-mp=gpu  -acc=gpu" )
