6a67591798510a139b8e588d70d6c8741c3302d2
Jorge <jg4@iastate.edu>
2026-03-30 23:33:34 -0500

    fix

--- cmake/compiler_flags_LLVMFlang_Fortran.cmake
+++ cmake/compiler_flags_LLVMFlang_Fortran.cmake
@@ -3,7 +3,7 @@ set(r8_flags "-fdefault-real-8") # Fortran flags for 64BIT precision
 set(r4_flags "-fdefault-real-4") # Fortran flags for 32BIT precision
 
 # LLVMFlang Fortran
-set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -ffree-line-length-none")
+set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS}")
 
 set(CMAKE_Fortran_FLAGS_RELEASE "-O2")
 set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -g")
